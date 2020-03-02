0faf335c894541b464832f7795e8b391bf9fa4f9
Minh Tran <mobject@gmail.com>
2020-03-02 13:40:02 -0600

    Spring bean util

--- src/main/java/mpplibrary/controller/ManageMemberController.java
+++ src/main/java/mpplibrary/controller/ManageMemberController.java
@@ -1,35 +1,13 @@
 package mpplibrary.controller;
 
+import mpplibrary.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class ManageMemberController {
-//	private ManageMemberDAO manageMemberDAO;
-//
-//	public ManageMemberController() {
-//		manageMemberDAO = new ManageMemberDAOImpl();
-//	}
-//
-//	public int insertMember(Member member) {
-//		return manageMemberDAO.insertMember(member);
-//	}
-//	public int updateMember(Member member) {
-//		return manageMemberDAO.insertMember(member);
-//	}
-//	public Member getById(String id) {
-//		return manageMemberDAO.getById(id);
-//	}
-//	public List<Member> getAll(){
-//		return manageMemberDAO.getAll();
-//	}
-//	public List<Member> getByFirstName(String firstName){
-//		return manageMemberDAO.getByFirstName(firstName);
-//	}
-//	public List<Member> getByLastName(String lastName){
-//		return manageMemberDAO.getByLastName(lastName);
-//	}
-//
-//	public int getLastestMemberId() {
-//		return manageMemberDAO.getLastestMemberId();
-//	}
-	
-	
-	
+
+    @Autowired
+    private UserService userService;
+
 }
